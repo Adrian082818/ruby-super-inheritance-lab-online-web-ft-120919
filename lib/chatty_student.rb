b473a78214d0319dfc93544b1b3db76ee22d7778
Adrian Mohabir <mohabiradrian@gmail.com>
2020-01-09 03:05:29 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,6 +2,6 @@ class ChattyStudent < Student
   
   def hello
     super 
-    puts "Hello there! I'm so excited to learn stuff.".phrase
+    puts "Hello there! I'm so excited to learn stuff."
   end 
 end 
